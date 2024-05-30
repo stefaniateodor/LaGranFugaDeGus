edae4bada9018137b6f067c010ac0e4d100a378d
Nathalie <nathalie.nazaryan@gmail.com>
2024-05-30 20:21:58 +0200

    nath

--- Assets/Scripts/fuegoscript.cs
+++ Assets/Scripts/fuegoscript.cs
@@ -31,6 +31,16 @@ public class fuegoscript : MonoBehaviour
        if(Time.time>=queHoraEs+tiempoDestruccion){
         Destroy(this.gameObject);
        }
-      
     }
+     void OnTriggerEnter2D(Collider2D col){
+    // Check if the collided object's name starts with "Cucaracha" or "Gusano"
+    if (col.gameObject.name.StartsWith("Cucaracha") || col.gameObject.name.StartsWith("Gusano")){
+        // Destroy the collided object
+        Destroy(col.gameObject);
+
+        // Destroy the object this script is attached to
+        Destroy(this.gameObject);
+    }
+}
+
 }
