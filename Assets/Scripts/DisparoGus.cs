0c74c429d6a2d079fb1d0c0c243c0e4a32a7b442
yassine <yassineourtanane@hotmail.fr>
2024-05-30 18:42:23 +0200

    disparo

--- Assets/Scripts/DisparoGus.cs
+++ Assets/Scripts/DisparoGus.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DisparoGus : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public  GameObject fuego;  // Start is called before the first frame update
     void Start()
     {
         
@@ -13,6 +13,9 @@ public class DisparoGus : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E)) {
+        Instantiate(fuego,transform.position, Quaternion.identity);
+
+       }
     }
 }
