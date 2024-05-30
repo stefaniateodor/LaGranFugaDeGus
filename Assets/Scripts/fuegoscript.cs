83470f6bb4a7bfd748c14ed95d9cb0caec3afa4c
yassine <yassineourtanane@hotmail.fr>
2024-05-30 19:15:22 +0200

    disparofinal

--- Assets/Scripts/fuegoscript.cs
+++ Assets/Scripts/fuegoscript.cs
@@ -7,20 +7,29 @@ public class fuegoscript : MonoBehaviour
     GameObject gus;
 
     bool bolaDerecha = true;
+    float tiempoDestruccion=3.0f;
+    public float speedBala=0.2f;
+    float queHoraEs;
     // Start is called before the first frame update
     void Start()
     {
        gus=GameObject.Find("Gus");
        bolaDerecha= gus.GetComponent<MovGus>().miraDerecha;
+       queHoraEs= Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-       if ( bolaDerecha ){
-         transform.Translate(0.002f,0,0); 
-       }else  {
-         transform.Translate(-0.002f,0,0); 
+      if (bolaDerecha){
+            transform.Translate(speedBala*Time.deltaTime,0,0,Space.World); 
+        } else {
+            transform.Translate((speedBala*Time.deltaTime)*-1,0,0,Space.World); 
+        }
+
+
+       if(Time.time>=queHoraEs+tiempoDestruccion){
+        Destroy(this.gameObject);
        }
       
     }
