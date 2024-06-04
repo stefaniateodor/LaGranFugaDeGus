2e893a8beebecefad4f0683c6a6f0f9690cf5465
Nathalie <nathalie.nazaryan@gmail.com>
2024-06-04 18:58:14 +0200

    a

--- Assets/Scripts/Peligro.cs
+++ Assets/Scripts/Peligro.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 
 
-public class Peligro : MonoBehaviour
+public class Peligro: MonoBehaviour
 {
     // Called when the character enters a trigger collider
     public Transform RespawnPoint;
