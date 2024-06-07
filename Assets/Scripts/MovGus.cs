d134791a04d4cba097f07b3b682580a53ad610ca
Nathalie <nathalie.nazaryan@gmail.com>
2024-06-07 12:47:39 +0200

    nat

--- Assets/Scripts/MovGus.cs
+++ Assets/Scripts/MovGus.cs
@@ -176,10 +176,6 @@ public class MovGus : MonoBehaviour
         }
     }
 
-<<<<<<< HEAD
-   
-}
-=======
     private void CollectSeed(GameObject seed)
     {
         seedCount++;
@@ -197,4 +193,3 @@ public class MovGus : MonoBehaviour
 
    
 }
->>>>>>> c86619033ab83acd23293f3c11a52b9d568945a2
